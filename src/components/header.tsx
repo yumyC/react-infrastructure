37902eb79797266f4561760e10d6627dfee221df
yummyc <yummyc@ciant.com>
2022-10-31 17:36:01 +0800

    Implement the header and the footer component.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import { Container,Grid, Menu, Image, Icon } from 'semantic-ui-react';
+import { useNavigate } from "react-router-dom";
+import './header.module.scss'
+import logo from '../assets/img/logo.png'
+import ChangeTheme from './changeTheme'
 
 function Header() {
+  const navigate = useNavigate();
+  const menuArray: any = [
+    {
+      'label': 'home',
+      'url': '/home'
+    },
+    {
+      'label': 'article',
+      'url': '/dashboard'
+    },
+    {
+      'label': 'about',
+      'url': '/about'
+    },
+  ]
   return (
-    <div>
-      Header
+    <div className="header-box">
+      <Container>
+        <Grid className='flex-space-between'>
+          <Menu secondary>
+            <a href='/home'><Image src={logo} wrapped ui={false} /></a>
+            {
+              menuArray.map((list, index) => {
+                return <Menu.Item key={list.label} name={list.label}
+                onClick={()=>navigate(list.url)}/>
+              })
+            }
+          </Menu>
+          <ChangeTheme/>
+        </Grid>
+      </Container>
     </div>
   );
 }
